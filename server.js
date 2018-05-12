4bf3f34eefb8622a2de9f0ec875473b69aee3313
Raj <kmrrajj@gmail.com>
2018-05-12 22:18:32 +0530

    added routes

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
-//DB config
-const db = require("./config/keys").mongoURI;
+// DB config
+const db = require('./config/keys').mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB connected!"))
+  .then(() => console.log('MongoDB connected!'))
   .catch(() => console.log(err));
 
-app.get("/", (req, res) => res.send("hi"));
+app.get('/', (req, res) => res.send('hi'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
