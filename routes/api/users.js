d0c855d72ddce373c2594220515977c06d0196aa
Raj <kmrrajj@gmail.com>
2018-05-13 16:31:46 +0530

    added gravatar configuration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 
 // Load User Model
 const user = require('../../models/User');
@@ -17,6 +18,11 @@ router.post('/register', (req, res) => {
     if (user) {
       return res.status(400).json({ email: 'Email already exists!' });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
