a445bd97246289deb3b2672e62fcd66cabb59ab8
Raj <kmrrajj@gmail.com>
2018-05-13 17:06:25 +0530

    added password hashing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // Load User Model
 const user = require('../../models/User');
@@ -28,6 +29,17 @@ router.post('/register', (req, res) => {
         email: req.body.email,
         avatar,
         password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          }
+        });
       });
     }
   });
